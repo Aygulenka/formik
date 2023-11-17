fd92a5e451104fb6e82edaf0dbcea38bff494759
Aygul Hurbanova <148243241+Aygulenka@users.noreply.github.com>
2023-11-17 22:39:50 +0400

    Create Aquarium.js
    exchange im aqurium

---
+++ src/Aquarium.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Aquarium = () => (
+  <div className="aquarium">
+    <div className="aquarium__table"></div>
+    <div className="aquarium__aquarium">
+      <div className="aquarium__water"></div>
+      <div className="aquarium__bubble"></div>
+      <div className="aquarium__bubble"></div>
+      <div className="aquarium__bubble"></div>
+      <div className="aquarium__bubble"></div>
+      <div className="aquarium__bubble"></div>
+      <div className="aquarium__bubble"></div>
+      <div className="aquarium__bubble"></div>
+      <div className="aquarium__bubble"></div>
+    </div>
+    <div className="aquarium__drops">
+      <div className="aquarium__drop"></div>
+      <div className="aquarium__water-column"></div>
+      <div className="aquarium__splash"></div>
+      <div className="aquarium__splash"></div>
+    </div>
+    <div className="aquarium__sponge-box">
+      <div className="aquarium__body">
+        <div className="aquarium__body-stripe"></div>
+        <div className="aquarium__body-hole"></div>
+      </div>
+      <div className="aquarium__face">
+        <div className="aquarium__mouth"></div>
+        <div className="aquarium__smile"></div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Aquarium;
